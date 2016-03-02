03e0369bed08f7650135be987a7dcb0a82404b4c
Billy Franz <billfranz00@gmail.com>
2016-03-01 20:55:59 -0800

    Third commit Current Time

--- js/go.js
+++ js/go.js
@@ -1,15 +1,39 @@
+function addZero(i) {
+	if(i < 10) {
+		return "0" + i;
+	}
+	else {
+		return i;
+	}
+}
+
+function twelve(hour) {
+	if(hour > 12) {
+		return hour = hour-12; //or h -= 12;
+	}
+	else if(hour === 0) {
+		return hour = 12;
+	}
+}
+
+function currentTime() {
+	var rightNow = new Date(),
+		hours = twelve(rightNow.getHours()),
+		minutes = addZero(rightNow.getMinutes()),
+		seconds = addZero(rightNow.getSeconds()),
+		theTime = hours + ":" + minutes + ":" + seconds;
+	document.getElementById("masterClock").innerHTML = theTime;
+	setTimeout(function() {
+		currentTime();
+	}, 1000);
+}
+
 function compareTime() {
-	var rightNow = new Date();
-	var hours = rightNow.getHours();
-	var minutes = rightNow.getMinutes();
-	if(hours > 12) {
-		hours = hours-12; //or h -= 12;
-	}
-	else if(hours === 0) {
-		hours = 12;
-	}
-	var clockValue = Number(document.getElementById("clockHour").value);
-	var minuteValue = Number(document.getElementById("clockMinute").value);
+	var rightNow = new Date(),
+		hours = twelve(rightNow.getHours()),
+		minutes = rightNow.getMinutes(),
+		clockValue = Number(document.getElementById("clockHour").value),
+		minuteValue = Number(document.getElementById("clockMinute").value);
 	if(hours === clockValue && minutes === minuteValue) {
 		alert("It's Time!!!!")
 	}
