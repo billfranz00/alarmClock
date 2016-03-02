c3d2be1cfd668802b60970fd8d10ad9f4c704474
Billy Franz <billfranz00@gmail.com>
2016-03-01 21:09:06 -0800

    Fourth Commit - the Alarm

--- js/go.js
+++ js/go.js
@@ -1,3 +1,4 @@
+//If number is a single digit, a zero is added in front (to make clock look better)
 function addZero(i) {
 	if(i < 10) {
 		return "0" + i;
@@ -7,6 +8,7 @@ function addZero(i) {
 	}
 }
 
+//Changes hour to am/pm (not internationl time)
 function twelve(hour) {
 	if(hour > 12) {
 		return hour = hour-12; //or h -= 12;
@@ -16,6 +18,7 @@ function twelve(hour) {
 	}
 }
 
+//Continuous Clock, updates every second
 function currentTime() {
 	var rightNow = new Date(),
 		hours = twelve(rightNow.getHours()),
@@ -28,6 +31,7 @@ function currentTime() {
 	}, 1000);
 }
 
+//Determines if inputted is same as current time
 function compareTime() {
 	var rightNow = new Date(),
 		hours = twelve(rightNow.getHours()),
@@ -35,9 +39,13 @@ function compareTime() {
 		clockValue = Number(document.getElementById("clockHour").value),
 		minuteValue = Number(document.getElementById("clockMinute").value);
 	if(hours === clockValue && minutes === minuteValue) {
+		//shows an alert when the times match
 		alert("It's Time!!!!")
 	}
 	else {
-		alert("Not time yet")
+		//runs function again if time inputted isn't the same as the actual time
+		setTimeout(function() {
+			compareTime();
+		}, 1000)
 	}
 }
